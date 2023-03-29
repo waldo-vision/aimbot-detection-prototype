205e1e38e4db68acf4d1051bd333ec5a89341b0c
yash.parge <yash_parge@yahoo.com.au>
2023-03-29 23:26:18 +1100

    Removed unncessary imports

--- save_cnn_output.py
+++ save_cnn_output.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
 import os
 import cv2
 import sys
